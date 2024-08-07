f5c92d71d330c521b2286e76a3c8415f23d27413
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2024-08-08 00:11:05 +0330

    create file of controller and userCn
    create file of controller and user controller

---
+++ Server/Controllers/UserCn.js
@@ -0,0 +1,5 @@
+export const login = catchAsync(async (req, res, next) => {});
+
+export const uploadProfile = catchAsync(async (req, res, next) => {});
+
+export const getAllUser = catchAsync(async (req, res, next) => {});
