d3b9f929d62d53b7189078f177757374d4e62a60
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2024-08-08 00:13:40 +0330

    Update MessageCn.js
    create base of message controller

---
+++ Server/Controllers/MessageCn.js
@@ -0,0 +1,5 @@
+export const sendMessage = catchAsync(async (req, res, next) => {});
+
+export const editMessage = catchAsync(async (req, res, next) => {});
+
+export const deletMessage = catchAsync(async (req, res, next) => {});
