6b117091e597a985d8911ff2a4a94fa5c1f0259e
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2024-08-08 00:18:09 +0330

    Update GroupConversationCn.js
    create group controller

---
+++ Server/Controllers/GroupConversationCn.js
@@ -0,0 +1,12 @@
+export const createGroup = catchAsync(async (req, res, next) => {});
+
+export const updateGroup = catchAsync(async (req, res, next) => {});
+
+export const deleteGroup = catchAsync(async (req, res, next) => {});
+
+export const addUser = catchAsync(async (req, res, next) => {});
+
+export const removeUser = catchAsync(async (req, res, next) => {});
+
+export const updatePermition = catchAsync(async (req, res, next) => {});
+
