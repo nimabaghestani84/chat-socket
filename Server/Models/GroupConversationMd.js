fdbe5820683d27528e543e9ac24115ddbdcfc5aa
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2024-08-08 10:44:39 +0330

    Update GroupConversationMd.js
    create GroupConversation

--- Server/Models/GroupConversationMd.js
+++ Server/Models/GroupConversationMd.js
@@ -1,11 +1,56 @@
 import mongoose from "mongoose";
 
-const GroupConversationSchema =new mongoose.Schema(
+const permissionSchima=new mongoose.Schema({
+    addAdmin:{
+        type:Boolean,
+        default:false
+    },
+    removeAdmin:{
+        type:Boolean,
+        default:false
+    },
+    addOrRemoveMember:{
+        type:Boolean,
+        default:true
+    }
+})
+const groupConversationSchema =new mongoose.Schema(
     {
-    
+        name:{
+            name:String,
+            required:[true,'name is required']
+        },
+        image:{
+            type:String
+        },
+        memebers:[{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        }],
+        messages:[{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Message'
+        }],
+        lastMessage:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Message'
+        },
+        admins:[
+            {
+                userId:{
+                    type:mongoose.Schema.Types.ObjectId,
+                    ref:'User'
+                },
+                Permission:permissionSchima
+            }
+        ],
+        isOwner:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        }
     },
     { timestamps: true }
 );
 
-const GroupConversation = mongoose.model('GroupConversation',GroupConversationSchema);
+const GroupConversation = mongoose.model('GroupConversation',groupConversationSchema);
 export default GroupConversation
