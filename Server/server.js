a01738fe1ed176efc71f03e78035f69a5af27337
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2024-08-08 11:42:38 +0330

    update app & server
    import server from socket
    &
    debug import socket yo index.js

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
-import app from "./app.js";
+import server from './Socket'
 import dotenv from "dotenv"
+import { server } from "./Socket/index.js";
 dotenv.config({path:'./config.env'})
 
 mongoose.connect(process.env.DATABASE_URL).then(()=>console.log('DB IS CONNECTED')).catch(err=>console.log(err))
 const port = process.env.PORT || 5001
-app.listen(port,()=>console.log(`Server is running on port ${port}`))
+server.listen(port,()=>console.log(`Server is running on port ${port}`))
