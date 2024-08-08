3b9fc92bc1005e5a97078a8d05cb275aba0d7117
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2024-08-08 10:44:19 +0330

    Update MessageMd.js
    create message model

--- Server/Models/MessageMd.js
+++ Server/Models/MessageMd.js
@@ -1,11 +1,25 @@
 import mongoose from "mongoose";
 
-const MessageSchema =new mongoose.Schema(
+const messageSchema =new mongoose.Schema(
     {
-    
+        content:{
+            type:String,
+            required:[true,'content is required']
+        },
+        attachment:{
+            type:String,
+        },
+        userId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        },
+        reply:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Message'
+        }
     },
     { timestamps: true }
 );
 
-const Message = mongoose.model('Message',MessageSchema);
+const Message = mongoose.model('Message',messageSchema);
 export default Message
