1f3b9a4068139e486b01756686e94c0c12edc6c5
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2024-08-08 11:33:03 +0330

    Update app.js
    update app.js

--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import path from 'path'
 import cors from 'cors'
+import { app } from './Socket'
 import { fileURLToPath } from 'url'
 import HandleError from './Utils/handleError.js'
 import catchError from './Utils/catchError.js'
@@ -9,7 +10,6 @@ import catchError from './Utils/catchError.js'
 const __filename=fileURLToPath(import.meta.url)
 export const __dirname =path.dirname(__filename)
 
-const app=express()
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.static('Public'))
@@ -19,6 +19,5 @@ app.use('*',(req,res,next)=>{
 return next(new HandleError('route not found',404))
 })
 app.use(catchError)
-
 
 export default app
