71075a4c1591c6473067b8263adb5e543f074033
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2024-08-08 10:43:45 +0330

    Update UserMd.js
    create user model

--- Server/Models/UserMd.js
+++ Server/Models/UserMd.js
@@ -1,11 +1,29 @@
 import mongoose from "mongoose";
 
-const UserSchema =new mongoose.Schema(
-    {
-    
-    },
-    { timestamps: true }
-);
+const contactSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+});
 
-const User = mongoose.model('User',UserSchema);
-export default User
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:[true,'username is unique'],
+        required:[true,'username is required']
+    },
+    phone:{
+        type:String,
+        unique:[true,'phone is unique'],
+        required:[true,'phone is required']
+    },
+    avatar:{
+        type:[String],
+    },
+    contact:{
+        type:[contactSchema],
+        default:[]
+    }
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+export default User;
