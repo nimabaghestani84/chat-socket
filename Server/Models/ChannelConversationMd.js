4d6341030eefbddc0046c461baf3d6cb5f809def
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2024-08-08 10:45:00 +0330

    Update ChannelConversationMd.js
    create ChannelConversation model

--- Server/Models/ChannelConversationMd.js
+++ Server/Models/ChannelConversationMd.js
@@ -1,11 +1,56 @@
 import mongoose from "mongoose";
 
-const ChannelConversationSchema =new mongoose.Schema(
+const permissionSchima=new mongoose.Schema({
+    addAdmin:{
+        type:Boolean,
+        default:false
+    },
+    removeAdmin:{
+        type:Boolean,
+        default:false
+    },
+    addOrRemoveMember:{
+        type:Boolean,
+        default:true
+    }
+})
+const channelConversationSchema =new mongoose.Schema(
     {
-    
+        name:{
+            name:String,
+            required:[true,'name is required']
+        },
+        image:{
+            type:String
+        },
+        memebers:[{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        }],
+        messages:[{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Message'
+        }],
+        lastMessage:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Message'
+        },
+        admins:[
+            {
+                userId:{
+                    type:mongoose.Schema.Types.ObjectId,
+                    ref:'User'
+                },
+                Permission:permissionSchima
+            }
+        ],
+        isOwner:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        }
     },
     { timestamps: true }
 );
 
-const ChannelConversation = mongoose.model('ChannelConversation',ChannelConversationSchema);
-export default ChannelConversation
+const GroupConversation = mongoose.model('GroupConversation',channelConversationSchema);
+export default GroupConversation
