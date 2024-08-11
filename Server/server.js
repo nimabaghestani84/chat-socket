d9321046f2ad057d16b0eb0496c5157b9e48cc3b
nimabaghestani84 <159420583+nimabaghestani84@users.noreply.github.com>
2024-08-12 02:20:27 +0330

    Update server.js
    chenge import from socket

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
-import server from './Socket'
+import server from './Socket/index.js'
 import dotenv from "dotenv"
-import { server } from "./Socket/index.js";
 dotenv.config({path:'./config.env'})
 
 mongoose.connect(process.env.DATABASE_URL).then(()=>console.log('DB IS CONNECTED')).catch(err=>console.log(err))
